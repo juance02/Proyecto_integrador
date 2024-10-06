64cc0289f59a5fefa097d25a70fc5992c43dff97
juance02 <juapceron1@misena.edu.co>
2024-10-06 09:29:02 -0500

    juance

--- principal/urls.py
+++ principal/urls.py
@@ -12,7 +12,7 @@ from principal.views import *
 
 urlpatterns = [
     path('', views.Inicio, name="Form"),
-    path('Registrarse/', views.register, name= 'registrarse'),
+    #path('Registrarse/', views.register, name= 'registrarse'),
     path('login/', LoginView.as_view(template_name='login/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='login/logout.html'), name='logout'),
     
--- principal/views.py
+++ principal/views.py
@@ -4,28 +4,14 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request, user) 
-            messages.success(request, f'Usuario {username} creado')         
-            return redirect('Form')
-            
-    else:
-        form = UserCreationForm()
-
-    return render(request, 'registration/register.html', {"form": form} )
-
-
 def Inicio(request):
     #product = producto.objects.all()
     #categorias=Categorias.objects.filter(activo=True)
     #context = { 'product': product,'categorias':categorias}
     return render(request, "index.html")
+
+
+
+
 
 
